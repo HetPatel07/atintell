fd3f03857f6b9a13f6cf981f3a91dd4146eecd42
hetpatel07 <hets7878401023@gmail.com>
2019-08-11 15:13:47 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
+    console.log(window.innerHeight);
+
     function updateBox(e) {
-        if (e.type == "progress") {
+        if (e.type == "progress" && window.innerWidth >= "1224") {
             $('#mainText').text(Math.floor((Math.random() * 15) + 1));
         } else {
             $('#mainText').text("15");
@@ -13,6 +15,15 @@ $(document).ready(function() {
     var windowHeight = parseInt($(window).innerHeight()) / 2;
     var firstTween = new TimelineMax();
     var speechTween = new TimelineMax();
+    var tranformTween = new TimelineMax();
+    var appHeight = new TimelineMax();
+    var appHeight1 = new TimelineMax();
+    var appHeight2 = new TimelineMax();
+    var appHeight3 = new TimelineMax();
+    var appHeight4 = new TimelineMax();
+    var appHeight5 = new TimelineMax();
+    var appliancesPart = $('.appliancesPart').width();
+    console.log(appliancesPart);
     firstTween
         .set(rule, {
             opacity: 0,
@@ -24,13 +35,34 @@ $(document).ready(function() {
         })
 
     speechTween
-        .set('.speech', {
-            transform: "translateX(50%)",
-        })
+    // .set('.speech', {
+    //     transform: "translateX(50%)",
+    // })
         .to('.speech', .5, {
-            transform: "translateX(0)",
-            opacity: 1
+        transform: "translateX(0)",
+        opacity: 1
+    })
+
+    tranformTween
+        .set('.wrapInner', {
+            x: '0',
+            transition: "1s",
         })
+        .to('.wrapInner', .01, {
+            // x: '-' + appliancesPart + 'px',
+            x: '-2670px',
+            transition: "1s"
+        })
+        // appHeight
+        //     .set('.appbox p', {
+        //         height: '0',
+        //         transition: "1s all",
+        //     })
+        //     .to('.appbox p', 1, {
+        //         // x: '-' + appliancesPart + 'px',
+        //         height: '203px',
+        //         transition: "1s all"
+        //     })
 
     var controller = new ScrollMagic.Controller();
 
@@ -55,8 +87,125 @@ $(document).ready(function() {
             offset: windowHeight,
         })
         .setTween(speechTween)
+        .addTo(controller);
+
+    // scene 4 start
+
+    var scene4 = new ScrollMagic.Scene({
+            triggerElement: '.wrapInner',
+            offset: 0,
+            duration: appliancesPart,
+            triggerHook: 0
+        })
+        .setTween(tranformTween)
+        .setPin(".wrapInner")
         .addIndicators()
         .addTo(controller);
+
+
+    // scene 5 start
+    var elemnts = $('.appbox p');
+    console.log(windowHeight);
+    var scene5 = new ScrollMagic.Scene({
+            triggerElement: '.wrapInner',
+            offset: windowHeight,
+            duration: "150px",
+            triggerHook: 0
+        })
+        .setTween(appHeight
+            .set(elemnts[0], {
+                height: '0',
+                transition: "1s all",
+            })
+            .to(elemnts[0], 1, {
+                height: '203px',
+            }))
+        .addIndicators()
+        .addTo(controller);
+
+    // scene 6
+
+    new ScrollMagic.Scene({
+            triggerElement: '.wrapInner',
+            offset: windowHeight + 150,
+            duration: "150px",
+            triggerHook: 0
+        })
+        .setTween(appHeight1
+            .set(elemnts[1], {
+                height: '0',
+            })
+            .to(elemnts[1], 1, {
+                height: '267px',
+            }))
+        .addIndicators()
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+            triggerElement: '.wrapInner',
+            offset: windowHeight + 300,
+            duration: "150px",
+            triggerHook: 0
+        })
+        .setTween(appHeight2
+            .set(elemnts[2], {
+                height: '0',
+            })
+            .to(elemnts[2], 1, {
+                height: '184px',
+            }))
+        .addIndicators()
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+            triggerElement: '.wrapInner',
+            offset: windowHeight + 450,
+            duration: "150px",
+            triggerHook: 0
+        })
+        .setTween(appHeight3
+            .set(elemnts[3], {
+                height: '0',
+            })
+            .to(elemnts[3], 1, {
+                height: '205px',
+            }))
+        .addIndicators()
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+            triggerElement: '.wrapInner',
+            offset: windowHeight + 600,
+            duration: "150px",
+            triggerHook: 0
+        })
+        .setTween(appHeight4
+            .set(elemnts[4], {
+                height: '0',
+            })
+            .to(elemnts[4], 1, {
+                height: '225px',
+            }))
+        .addIndicators()
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+            triggerElement: '.wrapInner',
+            offset: windowHeight + 750,
+            duration: "150px",
+            triggerHook: 0
+        })
+        .setTween(appHeight5
+            .set(elemnts[5], {
+                height: '0',
+            })
+            .to(elemnts[5], 1, {
+                height: '225px',
+            }))
+        .addIndicators()
+        .addTo(controller);
+
+
     TweenMax.to("#main", 1, {
         backgroundImage: "url(/img/bgmain.jpg)",
         transitionDelay: ".5s"
@@ -104,9 +253,9 @@ $(document).ready(function() {
 
     });
 
-    var wrapMainWidth = $('.wrapMain').width();
+    var wrapMainWidth = $('.wrapInner').width();
 
-    $('.wrapMain').css({
+    $('.wrapInner').css({
         'height': wrapMainWidth,
     })
     console.log(wrapMainWidth);
